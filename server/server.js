68ad6276c6be5f6585fbbbb6a719689494fc4e41
Alex-Coplin <alexander.coplin37@gmail.com>
2020-08-20 16:03:42 -0700

    Integrate and style all components

--- server/server.js
+++ server/server.js
@@ -4,7 +4,11 @@ var path = require('path');
 var cors = require('cors');
 
 App.get('/property/:listingId', (req, res) => {
-  res.sendFile('index.html', { root: path.join(__dirname, '../public') });
+  res.sendFile('index.html', { root: path.join(__dirname, '../public')});
+});
+
+App.get('/public/style.css', (req, res) => {
+  res.sendFile('style.css', {root: path.join(__dirname, '../public')});
 });
 
 //Reviews
@@ -25,13 +29,19 @@ App.get('/property/:ListingId/photos', (req, res) => {
   res.redirect(`http://localhost:3000/listing/${req.params.ListingId}/photos`);
 });
 
+//Checkin-Checkout
+App.get('/property/:id/rooms', (req, res) => {
+  console.log(req.params.id);
+  res.redirect(`http://localhost:5000/listing/${req.params.id}/rooms`);
+});
+
 //Description
 App.get('/property/:id/info', (req, res) => {
   res.redirect(`http://localhost:3333/listing/${req.params.id}/info`);
 });
 
 App.get('/property/:id/amenities', (req, res) => {
-  res.redirect(`http://localhost:3333/listing/${req.params.id}/info`);
+  res.redirect(`http://localhost:3333/listing/${req.params.id}/amenities`);
 });
 
 App.get('/property/:id/highlights', (req, res) => {
